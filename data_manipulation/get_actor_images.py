52a342434b6f533c083bcca11f8dab1fdd42f747
Flavio Gortana <flaviogortana@gmail.com>
2017-01-18 16:58:45 +0100

    added json data to git

--- data_manipulation/get_actor_images.py
+++ data_manipulation/get_actor_images.py
@@ -17,24 +17,34 @@ import json
 reload(sys)  
 sys.setdefaultencoding('utf-8')
 
-def fetch_image(actor):
-  if actor[9]:
-    wikipedia_id = urllib.quote(actor[9].encode('utf8'), ':/')
-    page_images_url = "https://de.wikipedia.org/w/api.php?action=query&titles=" + wikipedia_id + "&prop=pageimages&format=json&pithumbsize=600"
-    response = urllib.urlopen(page_images_url)
-    data = json.loads(response.read())
-    for page_id in data["query"]["pages"]:
-      page = data["query"]["pages"][page_id]
-      if 'thumbnail' in page:
-        urllib.urlretrieve(page['thumbnail']['source'], os.path.dirname(__file__) + "../data/images/actors_de/actor_" + actor[0] + ".jpg")
+num_images = 0
+
+def fetch_image(actor, actor_index):
+  wikipedia_id = actor[9][:-3]
+  wikipedia_id = urllib.quote(wikipedia_id.encode('utf8'), ':/')
+  page_images_url = "https://de.wikipedia.org/w/api.php?action=query&titles=" + wikipedia_id + "&prop=pageimages&format=json&pithumbsize=600"
+  response = urllib.urlopen(page_images_url)
+  data = json.loads(response.read())
+  for page_id in data["query"]["pages"]:
+    page = data["query"]["pages"][page_id]
+    if 'thumbnail' in page:
+      urllib.urlretrieve(page['thumbnail']['source'], os.path.dirname(__file__) + "../data/images/actors/actor_" + actor[0] + ".jpg")
+      print "Found image for: " + actor_id + " " + str(actor_index)
+      return True
+    else:
+      print "===> No image for: " + actor_id + " " + str(actor_index)
+      return False
 
 
-with open('../data/json/actors_wikipedia_de.json') as actors_file:
-  actors_data = json.load(actors_file)[:959]
+with open('../data/json/actors_wikipedia_merged.json') as actors_file:
+  actors_data = json.load(actors_file)
 
-print len(actors_data)
-
-for actor in actors_data:
+for i, actor in enumerate(actors_data):
   actor_id = actor[0]
-  print actor_id
-  fetch_image(actor)
+  image_found = fetch_image(actor, i)
+  if image_found:
+    num_images += 1
+
+print "Number of actors: "  + str(len(actors_data))
+print "Number of image: "  + str(num_images)
+print "Percentage of actors with image: " + str(float(num_images) / len(actors_data) * 100)
